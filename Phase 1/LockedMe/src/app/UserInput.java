f5c447af84f72cdb79219a5a72441456a71237e1
karmiris <79927820+karmiris@users.noreply.github.com>
2023-01-20 12:57:30 +0200

    Update UserInput.java

--- Phase 1/LockedMe/src/app/UserInput.java
+++ Phase 1/LockedMe/src/app/UserInput.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class UserInput {
 
 	static char GetCommand (int menu) {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
 		return '3';
 	}
 }
