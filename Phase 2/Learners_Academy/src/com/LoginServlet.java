123997bd400ffc3aba7b42cef456733b714e38c3
karmiris <79927820+karmiris@users.noreply.github.com>
2023-03-05 21:49:08 +0200

    Update LoginServlet.java

--- Phase 2/Learners_Academy/src/com/LoginServlet.java
+++ Phase 2/Learners_Academy/src/com/LoginServlet.java
@@ -1,5 +1,56 @@
 package com;
 
-public class LoginServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("pass");
+		if(emailid.equals("admin") && password.equals("admin123")) {
+	pw.println("Successfully login with get method");
+			rd1.forward(request, response);
+		}else {
+			pw.println("failure try once again with get method");
+			rd2.include(request, response);
+		}		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
+		String emailid = request.getParameter("emailid");
+		String password = request.getParameter("pass");
+		if(emailid.equals("admin") && password.equals("admin123")) {
+			pw.println("Successfully login with post method");
+			rd1.forward(request, response);
+		}else {
+			pw.println("failure try once again with post method");
+			rd2.include(request, response);
+		}
+	}
 
 }
