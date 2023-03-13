71a9d62b557eb0525634cfb69725da890c4bb5da
karmiris <79927820+karmiris@users.noreply.github.com>
2023-03-13 06:32:42 +0200

    Update Subject.java

--- Phase 2/Learners_Academy/src/main/java/com/entity/Subject.java
+++ Phase 2/Learners_Academy/src/main/java/com/entity/Subject.java
@@ -1,5 +1,17 @@
 package com.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Subject {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)	
+  private int id;
+  private String pname;
+  
+  
 }
